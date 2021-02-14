a496406f500b6987abb0897ad3adb8af483a947d
frankpagan <frank@cocreate.app>
2021-02-14 22:23:35 +0000

    clean tree

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -9,7 +9,7 @@ module.exports = {
     sources: [{
             path: "./docs/index.html",
             collection: "files",
-            document_id: "",
+            document_id: "6029a260e979fb17407e25cd",
             key: "html",
             data:{
                 name: "Sendgrid Doc",
@@ -19,10 +19,10 @@ module.exports = {
    
     crud: [{
             collection: "routes",
-            document_id: "",
+            document_id: "6029a260e979fb17407e25cc",
             data:{
                 collection: "files",
-                document_id: "",
+                document_id: "6029a260e979fb17407e25cd",
                 name: "html",
                 domains: ["cocreate.app", "server.cocreate.app", "ws.cocreate.app"],
                 route: "/docs/sendgrid",
