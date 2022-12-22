fda1a8b26be5ebae32361fa87ea76421fd23c890
Frank Pagan <frank@cocreate.app>
2022-12-22 13:22:47 +0000

    fix: update demos and bump dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
     libraryExport: "default",
     library: ["CoCreate", "sendgrid"],
     globalObject: "this",
-    // publicPath: 'https://server.cocreate.app/CoCreateJS/dist/'
   },
 
   plugins: [
