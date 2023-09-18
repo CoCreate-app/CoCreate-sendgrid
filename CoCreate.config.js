8a92a172e64398c039e447a46f1c890e736a64f7
frankpagan <frank@cocreate.app>
2023-09-17 19:11:38 -0600

    fix:  Add path and pathname

--- CoCreate.config.js
+++ CoCreate.config.js
@@ -8,17 +8,16 @@ module.exports = {
             "object": {
                 "_id": "6029a260e979fb17407e25cd",
                 "name": "index.html",
-                "path": "/docs/sendgrid/index.html",
+                "path": "/docs/sendgrid",
+                "pathname": "/docs/sendgrid/index.html",
                 "src": "{{./docs/index.html}}",
                 "host": [
                     "*",
                     "general.cocreate.app"
                 ],
-                "directory": "/docs/sendgrid",
-                "parentDirectory": "{{parentDirectory}}",
+                "directory": "sendgrid",
                 "content-type": "{{content-type}}",
-                "public": "true",
-                "website_id": "644d4bff8036fb9d1d1fd69c"
+                "public": "true"
             }
         }
     ]
