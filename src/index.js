54cc55f4b3aa61e1209065b320814950685fab40
frankpagan <frank@cocreate.app>
2026-02-09 11:13:20 -0600

    fix: root factory variable Module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(["./client.js"], function(CoCreateSendgrid) {
-        	return factory(CoCreateSendgrid)
+        define(["./client.js"], function(Module) {
+        	return factory(Module)
         });
     } else if (typeof module === 'object' && module.exports) {
-      const CoCreateSendgrid = require("./server.js")
-      module.exports = factory(CoCreateSendgrid);
+      const Module = require("./server.js")
+      module.exports = factory(Module);
     } else {
         root.returnExports = factory(root["./client.js"]);
   }
-}(typeof self !== 'undefined' ? self : this, function (CoCreateSendgrid) {
-  return CoCreateSendgrid;
+}(typeof self !== 'undefined' ? self : this, function (Module) {
+  return Module;
 }));
